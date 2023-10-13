a19b23c68d6035cc2bca64e91274f0b7840b3b8f
Usama <2021se41@student.uet.edu.pk>
2023-10-14 01:41:22 +0500

    Flutter API

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'Models.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<jasontodart> postlist = [];
+
+  Future<List<jasontodart>> getPostAPIs() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postlist.add(jasontodart.fromJson(i));
+      }
+      return postlist;
+    } else {
+      return postlist;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: getPostAPIs(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                return ListView.builder(
+                  itemCount: postlist.length,
+                  itemBuilder: (context, index) {
+                    return Text(postlist[index].toString());
+                  },
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
